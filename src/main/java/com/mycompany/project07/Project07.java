b34ef6182eb3c7b31d9f65af4eee8bd783229a2b
PedroMedeiros7631 <81970006+PedroMedeiros73423@users.noreply.github.com>
2024-09-21 15:05:37 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/project07/Project07.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.project07;
+import java.util.Scanner;
+
+public class Project07 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String nome = in.nextLine();
+        System.out.println("Infome o nome do usuário: " + nome);
+        
+    }
+}
